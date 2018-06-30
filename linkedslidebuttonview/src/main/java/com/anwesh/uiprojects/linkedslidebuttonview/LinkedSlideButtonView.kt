82308f1c1ee08a776c838da3542bc7a34f683202
Anwesh43 <anweshthecool0@gmail.com>
2018-06-30 11:46:44 +0530

    adding linkedslidebuttonview class

--- linkedslidebuttonview/src/main/java/com/anwesh/uiprojects/linkedslidebuttonview/LinkedSlideButtonView.kt
+++ linkedslidebuttonview/src/main/java/com/anwesh/uiprojects/linkedslidebuttonview/LinkedSlideButtonView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.linkedslidebuttonview
 /**
  * Created by anweshmishra on 30/06/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+
+class LinkedSlideButtonView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
