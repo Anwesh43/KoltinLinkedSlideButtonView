f76240582752080ad45c8e391a9e4b144aab2f27
Anwesh43 <anweshthecool0@gmail.com>
2018-06-30 15:51:46 +0530

    adding LinkedSlideButtonView in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedslidebuttonview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedslidebuttonview/MainActivity.kt
@@ -3,13 +3,17 @@ package com.anwesh.uiprojects.kotlinlinkedslidebuttonview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedslidebuttonview.LinkedSlideButtonView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedSlideButtonView.create(this)
+        val view : LinkedSlideButtonView = LinkedSlideButtonView.create(this)
+        view.addOnCompleteListener {
+            Toast.makeText(this, "animation ${it} completed", Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
