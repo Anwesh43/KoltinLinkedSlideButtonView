57081c7732512f1ec84d68f2caf8463027cfb6ee
Anwesh43 <anweshthecool0@gmail.com>
2018-06-30 12:22:36 +0530

    adding SBNode class

--- linkedslidebuttonview/src/main/java/com/anwesh/uiprojects/linkedslidebuttonview/LinkedSlideButtonView.kt
+++ linkedslidebuttonview/src/main/java/com/anwesh/uiprojects/linkedslidebuttonview/LinkedSlideButtonView.kt
@@ -9,6 +9,10 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.*
 
+val SB_NODES : Int = 5
+val buttonColor : Int = Color.parseColor("#f44336")
+val slideColor : Int = Color.parseColor("#F5F5F5")
+
 class LinkedSlideButtonView (ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -73,6 +77,60 @@ class LinkedSlideButtonView (ctx : Context) : View(ctx) {
             if (animated) {
                 animated = false
             }
+        }
+    }
+
+    data class SBNode (var i : Int, val state : State = State()) {
+
+        private var next : SBNode? = null
+
+        private var prev : SBNode? = null
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < SB_NODES - 1) {
+                next = SBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val gap : Float = w / SB_NODES
+            val btnW = gap/3
+            val getX : (Int) -> Float = {index -> (gap - btnW) * state.scales[index]}
+            val hBar : Float = h/10
+            canvas.save()
+            canvas.translate(i * gap, h/2)
+            paint.color = slideColor
+            canvas.drawRect(RectF(getX(2),-hBar/2, btnW + getX(0),hBar/2), paint)
+            paint.color = buttonColor
+            canvas.drawRect(RectF(getX(1), -hBar/2, getX(1) + btnW, hBar/2), paint)
+            canvas.restore()
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBNode {
+            var curr : SBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
